e66e5a1d4550fe2f31ffde97d339c1fccf4766fe
CandraMF <candragain99@gmail.com>
2025-08-11 22:48:38 +0700

    dix

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -13,6 +13,14 @@
     <meta name="robots" content="index, follow">
     <meta name="revisit-after" content="7 days">
 
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="apple-mobile-web-app-title" content="{{ $setting->nama }}">
+
     <!-- Open Graph Meta Tags -->
     <meta property="og:title" content="{{ $setting->nama }} - Layanan Ambulance Terpercaya di Bandung">
     <meta property="og:description" content="Layanan ambulance profesional 24 jam di Bandung untuk transport pasien, jenazah, dan situasi darurat medis">
